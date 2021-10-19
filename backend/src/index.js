15c5e0bbfeeeffc32a02903f1e9e55d57b374ba0
Guilherme Fernandes <57098649+guilfer-dev@users.noreply.github.com>
2021-10-19 19:42:35 -0300

    [feat] Upload
    Upload of repository once stored offline

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+}));
+app.use(express.json());
+app.use(routes);
+
+try {
+    app.listen(process.env.PORT || 3333)
+} catch (err) {
+    console.log(err)
+}
